288f3dca1005c1673f89414221749faf488ad1b8
Serger Rutrkovsky <dr.bibelman@gmai.com>
2023-04-24 15:02:22 +0300

    april 2023

---
+++ data.js
@@ -0,0 +1,17 @@
+const data = [
+    {
+        year: 2023,
+        month: 4,
+        waterСountFirst: 963,
+        waterСountSecond: 843,
+        gasCount: 3819,
+        electricityCount: 12596,
+        comPaid: 88.52,
+        phonPaid: 3.73,
+        electricityPaid: 33.93,
+        tvPaid:7,
+        gasPaid: 6.57,
+        internetPaid: 30,
+    }
+];
+export default data;
